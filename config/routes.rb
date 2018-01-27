3ac48bf4f2ac8f0b5d6f29f8a37857244a906a5f
briankeegan <brian.morris.keegan@gmail.com>
2018-01-27 13:30:58 -0500

    Update sign-out chane-password to accept id para
      -  add to routes:
      - `delete '/sign-out/:id' => 'users#signout''`
      - `patch '/change-password/:id' => 'users#changepw'`

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
   delete '/sign-out' => 'users#signout'
+  delete '/sign-out/:id' => 'users#signout'
   patch '/change-password' => 'users#changepw'
+  patch '/change-password/:id' => 'users#changepw'
   resources :users, only: %i[index show]
 end
